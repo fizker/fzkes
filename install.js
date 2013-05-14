4ce6769e7f0a0584d82183267517297cb0885298
Benjamin Horsleben <benjamin@fizkerinc.dk>
2013-05-14 08:50:37 +0200

    Updated install script to function even if the test folder is not present (it does not get published to npm). Moved glob dependency to devDependencies, as it is only used to prepare browser tests anyway.

--- install.js
+++ install.js
@@ -1,25 +1,35 @@
 #!/usr/bin/env node
-var glob = require('glob')
 var fs = require('fs')
-
-var files = glob.sync('test/*/**/*.js')
-var scripts = files
-	.map(function(f) {
-		return '<script src=' + f.replace(/^test\//, '') + '></script>'
-	})
-	.join('\n')
-var tmpl = fs.readFileSync('./test/browser.mustache', 'utf8')
-var html = tmpl.replace('{{{scripts}}}', scripts)
-fs.writeFileSync('./test/browser.html', html)
-
 var browserify = require('browserify')
-browserify()
-	.require('./index.js', { expose: 'fzkes' })
-	.bundle()
-	.pipe(fs.createWriteStream('browser.js'))
 
-browserify()
-	.external('./index.js')
-	.add('./test/common.js')
-	.bundle()
-	.pipe(fs.createWriteStream('browser-test.js'))
+browser()
+
+if(fs.existsSync('test')) {
+	browserTests()
+}
+
+function browser() {
+	browserify()
+		.require('./index.js', { expose: 'fzkes' })
+		.bundle()
+		.pipe(fs.createWriteStream('browser.js'))
+}
+
+function browserTests() {
+	var glob = require('glob')
+	var files = glob.sync('test/*/**/*.js')
+	var scripts = files
+		.map(function(f) {
+			return '<script src=' + f.replace(/^test\//, '') + '></script>'
+		})
+		.join('\n')
+	var tmpl = fs.readFileSync('./test/browser.mustache', 'utf8')
+	var html = tmpl.replace('{{{scripts}}}', scripts)
+	fs.writeFileSync('./test/browser.html', html)
+
+	browserify()
+		.external('./index.js')
+		.add('./test/common.js')
+		.bundle()
+		.pipe(fs.createWriteStream('browser-test.js'))
+}
